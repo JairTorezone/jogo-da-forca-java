fd5a7e657b05f1970339972c08a228cf2d8536d3
Jair Torezone <jairtorezone@gmail.com>
2022-01-26 09:56:50 -0300

    Created the GameException class and the txt word file in resource

---
+++ src/main/java/br/com/torezone/jogoforca/game/GameException.java
@@ -0,0 +1,12 @@
+package br.com.torezone.jogoforca.game;
+
+public class GameException extends RuntimeException {
+
+	private static final long serialVersionUID = 9026766083996945252L;
+
+	public GameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
