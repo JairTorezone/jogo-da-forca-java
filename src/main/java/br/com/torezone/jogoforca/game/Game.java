66273b365c32194d880860cda9a6bc047cd2b975
Jair Torezone <jairtorezone@gmail.com>
2022-01-26 09:29:41 -0300

    Testing the Word Class Methods

--- src/main/java/br/com/torezone/jogoforca/game/Game.java
+++ src/main/java/br/com/torezone/jogoforca/game/Game.java
@@ -1,8 +1,16 @@
 package br.com.torezone.jogoforca.game;
 
+import br.com.torezone.jogoforca.core.Word;
+
 public class Game {
 
 	public void start() {
 		
+		Word word = new Word("Casa");
+		word.hasChar('a');
+		word.hasChar('c');
+		word.hasChar('s');
+		System.out.println(word.discovered());
+		System.out.println(word);
 	}
 }
