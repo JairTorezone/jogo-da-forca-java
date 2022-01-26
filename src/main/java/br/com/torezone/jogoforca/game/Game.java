e8e501dada7ef4b7b513a523006f2baf3981c137
Jair Torezone <jairtorezone@gmail.com>
2022-01-26 10:14:27 -0300

    Tested the Singleton pattern

--- src/main/java/br/com/torezone/jogoforca/game/Game.java
+++ src/main/java/br/com/torezone/jogoforca/game/Game.java
@@ -1,16 +1,19 @@
 package br.com.torezone.jogoforca.game;
 
+import br.com.torezone.jogoforca.core.Dictionary;
 import br.com.torezone.jogoforca.core.Word;
 
 public class Game {
 
 	public void start() {
 		
-		Word word = new Word("Casa");
-		word.hasChar('a');
-		word.hasChar('c');
-		word.hasChar('s');
-		System.out.println(word.discovered());
-		System.out.println(word);
+		Dictionary d1 = Dictionary.getInstance();
+		Dictionary d2 = Dictionary.getInstance();
+		System.out.println(d1);
+		System.out.println(d2);
+		
+		Word w = d1.nextWord();
+		System.out.println(w.getOriginalWord());
+		
 	}
 }
