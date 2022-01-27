51e667f3336ab9cde5390b6dd12eae4fd4dd8989
Jair Torezone <jairtorezone@gmail.com>
2022-01-27 20:32:58 -0300

    Created exception class InvalidCharacterException

---
+++ src/main/java/br/com/torezone/jogoforca/core/InvalidCharacterException.java
@@ -0,0 +1,12 @@
+package br.com.torezone.jogoforca.core;
+
+@SuppressWarnings("serial")
+public class InvalidCharacterException extends Exception {
+
+
+	public InvalidCharacterException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
